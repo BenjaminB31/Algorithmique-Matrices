d565ce7a8ee6081528241e818da24b7d294e2bc5
Benjamin Blanchard <56205597+BenjaminB31@users.noreply.github.com>
2020-10-09 17:17:55 +0200

    Update index.php
    UPDATE mise en forme de tableau

--- index.php
+++ index.php
@@ -8,21 +8,26 @@ $matrice[4] = array(1,2,3,4,5);
 
 $ligne = "0";
 
+echo "<table>";
+
 while (isset($matrice[$ligne])) {
     
     $colone = "0";
-    
+    echo "<tr>";
     while (isset($matrice[$ligne][$colone])) {
     
+        echo "<td>";
         echo $matrice[$ligne][$colone];
-        
+        echo "</td>";
         $colone = $colone + "1";
     
     }
     
-    echo "</br>";
+    echo "</tr>";
     $ligne = $ligne + "1";
     
 }
+
+echo "</table>";
 
 ?>
