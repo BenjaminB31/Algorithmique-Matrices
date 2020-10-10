d2e82c6ed79401aa2eb2334572a433636c0731dd
Benjamin B <56205597+BenjaminB31@users.noreply.github.com>
2020-10-10 08:48:27 +0200

    Update index.php
    Update value in color

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $matrice = array();
-$matrice[0] = array(1,2,3,4,5);
-$matrice[1] = array(1,2,3,4,5);
-$matrice[2] = array(1,2,3,4,5);
-$matrice[3] = array(1,2,3,4,5);
-$matrice[4] = array(1,2,3,4,5);
+$matrice[0] = array(0,25,50,75,100);
+$matrice[1] = array(0,25,50,75,100);
+$matrice[2] = array(0,25,50,75,100);
+$matrice[3] = array(0,25,50,75,100);
+$matrice[4] = array(0,25,50,75,100);
 
 $ligne = "0";
 
@@ -13,12 +13,11 @@ echo "<table>";
 while (isset($matrice[$ligne])) {
     
     $colone = "0";
-    echo "<tr>";
+    echo "<tr style='height: 100px;'>";
     while (isset($matrice[$ligne][$colone])) {
-    
-        echo "<td>";
-        echo $matrice[$ligne][$colone];
-        echo "</td>";
+        $color = $matrice[$ligne][$colone];
+        $color = 100+(-1*$color);
+        echo "<td style='background-color:hsl(11, 0%, $color%);width: 100px;'></td>";
         $colone = $colone + "1";
     
     }
